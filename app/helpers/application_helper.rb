a5c1d5fe901caa285bfff53ea4a97a2c247dd477
Maximilian Richt <maxi@richt.name>
2014-09-18 00:56:07 +0200

    Basic styling, more information text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def link_to_paper(paper, html_options={})
-    link_to paper.title, paper_path(paper.body, paper.legislative_term, paper), html_options
+  def link_to_paper(title, paper, html_options={})
+    link_to title, paper_path(paper.body, paper.legislative_term, paper), html_options
   end
 
 end
