4da132e0c09d7757486d5b598eb485f39307a3bb
Maximilian Richt <maxi@richt.name>
2014-09-18 00:54:03 +0200

    Remove pdf mime registration, already in action_dispatch

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,4 +2,4 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.register "application/pdf", :pdf
+# Mime::Type.register "application/pdf", :pdf # already in action_dispatch
