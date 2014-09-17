9b7d7b61b6ed38658346922978a1d93149910199
Maximilian Richt <maxi@richt.name>
2014-09-18 00:56:25 +0200

    Less papers on start page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
 class SiteController < ApplicationController
   def index
-    @papers = Paper.limit(15).order(published_at: :desc, reference: :desc)
+    @papers = Paper.limit(10).order(published_at: :desc, reference: :desc)
   end
 end
