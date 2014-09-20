d5d144028ee6f5dba34ebd44021118627e9ec2ed
Maximilian Richt <maxi@richt.name>
2014-09-20 13:42:30 +0200

    Use correct german grammar for time_ago_in_words on start page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     link_to title, paper_path(paper.body, paper.legislative_term, paper), html_options
   end
 
+  def time_ago_in_words_dativ(time)
+    time_ago_in_words(time, scope: :'datetime.distance_in_words_dativ')
+  end
+
 end
