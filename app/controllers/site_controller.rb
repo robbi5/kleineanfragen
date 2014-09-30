d05fbe92b527574953afb6899a7bf2c7b7c7110c
Maximilian Richt <maxi@richt.name>
2014-09-30 18:39:21 +0200

    List of bodies on start page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,6 @@
 class SiteController < ApplicationController
   def index
+    @bodies = Body.order(name: :asc).all
     @papers = Paper.limit(10).order(published_at: :desc, reference: :desc)
   end
 end
