2982278c690e5b339f766429fcaa1e1ba135dd7d
Maximilian Richt <maxi@richt.name>
2014-12-30 12:40:57 +0100

    Rewrite Brandenburg scraper, add details

--- app/scrapers/brandenburg_landtag_scraper.rb
+++ app/scrapers/brandenburg_landtag_scraper.rb
@@ -3,47 +3,80 @@ require 'date'
 module BrandenburgLandtagScraper
   BASE_URL = 'http://www.parldok.brandenburg.de'
 
+  # TODO: Parse and use RSS?
+  # http://www.parldok.brandenburg.de/starweb/LTBBRSS/servlet.starweb?path=LTBBRSS/LTBBProfilRSS.web&format=DokumentUP&search=%28%28%2fWP+6%29+AND+%28%2fDART%2cDARTS%2cETYPF%2cETYP2F%2cDTYPF%2cDTYP2F+%28%22KLEINE+ANFRAGE%22+OR+%22GRO%DFE+ANFRAGE%22%29%29+AND+TYP%3dDOKDBE%29&title=Anfragen&x=x.xml
+  # -> ((/WP 6) AND (/DART,DARTS,ETYPF,ETYP2F,DTYPF,DTYP2F ("KLEINE ANFRAGE" OR "GROßE ANFRAGE")) AND TYP=DOKDBE)
+  # Nope, no pagination
+
   class Overview < Scraper
     # SEARCH_URL = BASE_URL + '/starweb/LTBB/servlet.starweb?path=LTBB/lisshfl.web&id=ltbbfastlink&search=WP%3d5+AND+%28DTYP%3dKleine+Anfrage%29&format=WEBVORGLFL'
-    SEARCH_URL = BASE_URL + '/starweb/LTBB/servlet.starweb?path=LTBB/lisshfl.web&id=LTBBFASTLINK&search=TODAY%3dX+AND+WP%3d5+AND+%28DTYP%3dKleine+Anfrage%29&format=WEBVORGLFL'
+    SEARCH_URL = BASE_URL + '/starweb/LTBB/servlet.starweb?path=LTBB/lisshfl.web&id=LTBBFASTLINK&format=WEBVORGLFL&search='
 
     def scrape
-      m = Mechanize.new
-      mp = m.get SEARCH_URL
+      # FIXME: today?!, pagination?
+      mp = mechanize.get SEARCH_URL + CGI.escape('TODAY=X AND WP=' + @legislative_term + ' AND (DTYP=Kleine Anfrage)')
+      body = mp.search "//table[contains(@summary, 'Hauptbereich')]"
 
-      body = mp.search "//div[contains(@name, 'IfReportGenerated')]"
-      legterm = body.search("//th[contains(@class, 'gross2')]").inner_html.strip
-      legislative_term = legterm.match(/(\d+). Wahlperiode/)[1]
       papers = []
-
-      body.css('>table').each do |item|
-        link = item.at_css("a[href*='parladoku']")
-        next if link.nil?
-
-        title_el = item.at('.//th/../following-sibling::tr[1]/td[2]')
+      body.css('table td.klein').each do |item|
+        title_el = item.parent.next_element.next_element
         title = title_el.text
 
-        row = link.previous.text
-        data = row.strip.match(/(.+) \((.+)\)\s+([\d\.]+) Drs$/)
+        data_el = title_el.next_element.at_css('table td+td')
+        link = data_el.at_css('a')
         path = link.attributes['href'].value
+        url = Addressable::URI.parse(BASE_URL + path).normalize.to_s
         full_reference = link.text
-        originators = { people: data[1], party: data[2] }
-        date = data[3]
+        reference = full_reference.split('/').last
+        # KlAnfr 123 Aaaaaa Bbbbbb (ABC), Cccccc Ddddddd (ABC) 11.12.2014 Drs 6/123 (1 S.)
+        meta = data_el.text.strip.match(/\s(\D+ \(.+\),?)\s+([\d\.]+) Drs/)
+        originators = NamePartyExtractor.new(meta[1]).extract
+        date = meta[2]
         published_at = Date.parse(date)
-
-        url =  Addressable::URI.parse(BASE_URL + path).normalize.to_s
-
         papers << {
-          legislative_term: legislative_term,
+          legislative_term: @legislative_term,
           full_reference: full_reference,
+          reference: reference,
           title: title,
           url: url,
           published_at: published_at,
           originators: originators
         }
       end
-
       papers
+    end
+  end
+
+  class Detail < Scraper
+    # using rss/xml export
+    SEARCH_URL = BASE_URL + '/starweb/LTBBRSS/servlet.starweb?path=LTBBRSS/LTBBProfilRSS.web&format=DokumentUP&search='
+
+    def initialize(legislative_term, reference)
+      @legislative_term = legislative_term
+      @reference = reference
+    end
+
+    def full_reference
+      @legislative_term.to_s + '/' + @reference.to_s
+    end
+
+    def scrape
+      mp = mechanize.get SEARCH_URL + CGI.escape('DART=D AND WP=' + @legislative_term + ' AND DNR,KORD=' + @reference)
+      title = mp.search('//title').first.text
+      desc = mp.search('//description').first.text
+      # Kleine Anfrage 123 Aaaaaaaa Bbbbbbbb (Ccccc) Drucksache 1/222 10.11.2014 (2 S.)
+      _, person, party, date = title.match(/\d+\s(\D+)\s\((.*)\)\s.*\s([\d\.]+)\s/).to_a
+      _, path = desc.match(/\shref="(.*)"\s/).to_a
+      url = Addressable::URI.parse(BASE_URL + path).normalize.to_s
+      {
+        legislative_term: @legislative_term,
+        full_reference: full_reference,
+        reference: @reference,
+        title: title,
+        published_at: Date.parse(date),
+        url: url,
+        originators: { people: [person], parties: [party] }
+      }
     end
   end
 end
