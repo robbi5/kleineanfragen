041ac6da7f77c6a0bc548e3728eac91d4cc2987d
Maximilian Richt <maxi@richt.name>
2014-12-30 12:32:12 +0100

    Add single paper scraper for berlin

--- app/scrapers/berlin_agh_scraper.rb
+++ app/scrapers/berlin_agh_scraper.rb
@@ -6,7 +6,7 @@ module BerlinAghScraper
   class Overview < Scraper
     SEARCH_URL = BASE_URL + '/starweb/AHAB/servlet.starweb?path=AHAB/lisshfl.web&id=ahabfastlink&format=WEBVORGLFL&search='
 
-    # FIXME: add support for pagination
+    # FIXME: find search with pagination, add support for pagination
     def scrape
       mp = mechanize.get SEARCH_URL + CGI.escape('WP=' + @legislative_term + ' AND (etyp=schriftl*)')
 
@@ -65,9 +65,69 @@ module BerlinAghScraper
       papers
     end
   end
+
+  class Details < Scraper
+    SEARCH_URL = BASE_URL + '/starweb/AHAB/servlet.starweb?path=AHAB/lisshfl.web&id=ahabfastlink&format=WEBVORGLFL&search='
+
+    def initialize(legislative_term, reference)
+      @legislative_term = legislative_term
+      @reference = reference
+    end
+
+    def full_reference
+      @legislative_term.to_s + '/' + @reference.to_s
+    end
+
+    def scrape
+      mp = mechanize.get SEARCH_URL + CGI.escape('WP=' + @legislative_term + ' AND DNR=' + @reference)
+      body = mp.search "//table[contains(@summary, 'Hauptbereich')]"
+      item = body.search('//td[contains(@colspan, 3)]')
+      title_el = item.search('../following-sibling::tr[1]/td[2]/b')
+      title = title_el.inner_html.gsub(/\<br\>/, ' ')
+
+      # FIXME duplicate code, cleanup!
+      # we hit the subtitle row
+      if container.search('a').length == 0
+        container = item.search('../following-sibling::tr[5]/td[2]')
+
+        # and now the location row
+        if container.search('a').length == 0
+          container = item.search('../following-sibling::tr[6]/td[2]')
+        end
+      end
+
+      # the first "Drucksache 17/1234" link
+      link = container.search('a')[0]
+
+      if link.nil? || container.search('a').length == 0
+        # skip broken records (no pdf)
+        fail 'link_el not found'
+      end
+
+      names = link.previous_element.previous.text
+      originators = NamePartyExtractor.new(names).extract
+      path = link.attributes['href'].value
+      full_reference = link.text.match(/([\d\/]+)/)[1]
+      date = container.text.match(/.*vom ([\d\.]+)/m)[1]
+      published_at = Date.parse(date)
+
+      url =  Addressable::URI.parse(BASE_URL + path).normalize.to_s
+
+      {
+        legislative_term: @legislative_term,
+        full_reference: full_reference,
+        reference: @reference,
+        title: title,
+        url: url,
+        published_at: published_at,
+        originators: originators
+      }
+    end
+  end
 end
 
 ###
 # Usage:
 #   puts BerlinAghScraper::Overview.new.scrape.inspect
+#   puts BerlinAghScraper::Detail.new(17, 2000).scrape.inspect
 ###
