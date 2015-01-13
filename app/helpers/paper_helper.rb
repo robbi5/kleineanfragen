b678e8682bd020abc91efd8c2513a8d80b84322c
Maximilian Richt <maxi@richt.name>
2015-01-13 08:03:30 +0100

    Search: fix query *, fix .try(:fetch,...)

--- app/helpers/paper_helper.rb
+++ app/helpers/paper_helper.rb
@@ -1,7 +1,7 @@
 module PaperHelper
   def facet_count(key, term)
-    terms = @papers.facets[key].try(:fetch, 'terms')
-    terms.find { |el| el['term'] == term }.try(:fetch, 'count') unless terms.nil?
+    terms = @papers.facets[key].try(:fetch, 'terms', nil)
+    terms.find { |el| el['term'] == term }.try(:fetch, 'count', nil) unless terms.nil?
   end
 
   def write_facet_count(key, term)
