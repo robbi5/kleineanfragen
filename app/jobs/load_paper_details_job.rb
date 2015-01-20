ac87e002dd41638ef3bc4ad2acb48a216d678eaa
Maximilian Richt <maxi@richt.name>
2015-01-20 21:53:59 +0100

    Enable LoadPaperDetails to overwrite empty/forced keys

--- app/jobs/load_paper_details_job.rb
+++ app/jobs/load_paper_details_job.rb
@@ -1,15 +1,18 @@
 class LoadPaperDetailsJob < ActiveJob::Base
   queue_as :meta
 
+  OVERWRITEABLE = [:originators, :answerers]
+
   def perform(paper)
     return unless paper.body.scraper.const_defined? :Detail
 
     Rails.logger.info "Loading details for Paper [#{paper.body.state} #{paper.full_reference}]"
     detail = paper.body.scraper::Detail.new(paper.legislative_term, paper.reference).scrape
 
-    # FIXME: should iterate over detail, fill anything that is nil in paper
+    detail.each do |key, value|
+      paper.send("#{key}=", value) if paper.send(key).blank? || OVERWRITEABLE.include?(key)
+    end
 
-    paper.originators = detail[:originators]
     paper.save
   end
 end
