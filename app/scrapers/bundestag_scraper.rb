611b9269b9ccd0bf20327c13338e43bfafefef85
Maximilian Richt <maxi@richt.name>
2015-01-24 20:54:05 +0100

    BT: fix missing title

--- app/scrapers/bundestag_scraper.rb
+++ app/scrapers/bundestag_scraper.rb
@@ -120,7 +120,7 @@ module BundestagScraper
       urheber = node.at_css('URHEBER').text
       if urheber.starts_with? 'Kleine Anfrage'
         node.css('PERSOENLICHER_URHEBER').each do |unode|
-          originators[:people] << "#{unode.at_css('VORNAME').text} #{unode.at_css('NACHNAME').text}"
+          originators[:people] << "#{unode.at_css('PERSON_TITEL').try(:text)} #{unode.at_css('VORNAME').text} #{unode.at_css('NACHNAME').text}".strip
           party = unode.at_css('FRAKTION').text
           originators[:parties] << party unless originators[:parties].include? party
         end
