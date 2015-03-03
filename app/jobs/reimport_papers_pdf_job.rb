c45dea7ae1dde63657c344a36e4509180fb9f5ab
Maximilian Richt <maxi@richt.name>
2015-03-03 08:44:44 +0100

    Redownload BT papers that may be replaced by an proofread version
    „Vorabfassung - wird durch die lektorierte Version ersetzt.“

---
+++ app/jobs/reimport_papers_pdf_job.rb
@@ -0,0 +1,15 @@
+class ReimportPaperPDFJob < ActiveJob::Base
+  queue_as :meta
+
+  DISTANCE = 5.days
+
+  def perform
+    bt = Body.find_by_state('BT')
+    papers = Paper.where(body: bt).where(['pdf_last_modified < ?', Date.today - DISTANCE]).where("contents LIKE '%Korrektur\nDrucksache%'")
+
+    papers.each do |paper|
+      logger.info "Adding reimport of Paper [#{paper.body.state} #{paper.full_reference}] to queue"
+      StorePaperPDFJob.perform_later(paper, force: true)
+    end
+  end
+end
