cfb8fd1369e052921aff76a1611b842fd4840ec7
Maximilian Richt <maxi@richt.name>
2015-03-12 11:19:06 +0100

    Enable URL generation for email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] || 'localhost' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
 
