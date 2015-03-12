0bc3639543437cf7d19546554dda78fb22de1065
Maximilian Richt <maxi@richt.name>
2015-03-12 11:36:16 +0100

    Fix email tests, access report like an hash again

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,16 +4,4 @@ class Report < Hash
     send(:[]=, :created_ip, created_ip)
     send(:[]=, :user_agent, user_agent)
   end
-
-  def created_at
-    try(:[], :created_at)
-  end
-
-  def created_ip
-    try(:[], :created_ip)
-  end
-
-  def user_agent
-    try(:[], :user_agent)
-  end
 end
--- test/mailers/opt_in_mailer_test.rb
+++ test/mailers/opt_in_mailer_test.rb
@@ -7,17 +7,17 @@ class OptInMailerTest < ActionMailer::TestCase
   end
 
   test 'opt_in' do
-    opt_in = OptIn.new(email: 'test@example.org')
-    subscription = Subscription.new
+    opt_in = opt_ins(:opt_in_unconfirmed)
+    subscription = subscriptions(:subscription_inactive)
     mail = OptInMailer.opt_in(opt_in, subscription)
     assert_equal 'kleineAnfragen - E-Mail-Adresse bestätigen', mail.subject
-    assert_equal ['test@example.org'], mail.to
+    assert_equal ['hello@example.com'], mail.to
     assert_equal [@system_address], mail.from
     # assert_match 'Hi', mail.body.encoded # FIXME: add things from email body
   end
 
   test 'report' do
-    opt_in = OptIn.new(email: 'test@example.org')
+    opt_in = opt_ins(:opt_in_unconfirmed)
     report = Report.new
     mail = OptInMailer.report(opt_in, report)
     assert_equal "[report] #{opt_in.email} ist nun auf der E-Mail-Blacklist", mail.subject
