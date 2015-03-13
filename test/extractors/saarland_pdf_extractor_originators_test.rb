47f81b551c54e769d6ebfeee1f43af5d83a683f5
Andreas Worm <andreas.worm@headissue.com>
2015-03-13 10:17:59 +0100

    fix saarland extractor

--- test/extractors/saarland_pdf_extractor_originators_test.rb
+++ test/extractors/saarland_pdf_extractor_originators_test.rb
@@ -12,12 +12,31 @@ class SaarlandPDFExtractorOriginatorsTest < ActiveSupport::TestCase
   end
 
   test 'one person, single line 2' do
-    paper = Struct.new(:contents).new("Anfrage der Abgeordneten Astrid Schramm (DIE LINKE.) \n")
+    paper = Struct.new(:contents).new("Anfrage der Abgeordneten Astrid Schramm (DIE LINKE.)")
 
     originators = SaarlandPDFExtractor.new(paper).extract_originators
 
     assert_equal 1, originators[:people].size
     assert_equal 'Astrid Schramm', originators[:people].first
     assert_equal 'DIE LINKE.', originators[:parties].first
+  end
+
+  test 'one person, multiline' do
+    paper = Struct.new(:contents).new("Anfrage der Abgeordneten Astrid\nSchramm (DIE LINKE.)")
+
+    originators = SaarlandPDFExtractor.new(paper).extract_originators
+
+    assert_equal 1, originators[:people].size
+    assert_equal 'Astrid Schramm', originators[:people].first
+    assert_equal 'DIE LINKE.', originators[:parties].first
+  end
+
+  test 'one person, whole' do
+    paper = Struct.new(:contents).new(File.read(Rails.root.join('test/fixtures/saarland_paper.txt')))
+    originators = SaarlandPDFExtractor.new(paper).extract_originators
+
+    assert_equal 1, originators[:people].size
+    assert_equal 'Michael Neyses', originators[:people].first
+    assert_equal 'B90/Grüne', originators[:parties].first
   end
 end
