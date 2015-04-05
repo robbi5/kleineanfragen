ed7b01eb5fab0d930de594707bb9afa41751ae73
Maximilian Richt <maxi@richt.name>
2015-04-05 11:48:57 +0200

    Scraper Results: display always the last 10 days

--- app/controllers/scraper_results_controller.rb
+++ app/controllers/scraper_results_controller.rb
@@ -4,14 +4,10 @@ class ScraperResultsController < ApplicationController
   def index
     @bodies = Body.all
     @results = {}
+    @dates = ((Date.today - 10.days)..Date.today).to_a
     @bodies.each do |body|
-      @results[body.id] = body.scraper_results.order(created_at: :desc).limit(10).reverse
-    end
-
-    @dates = @results.map { |_, results| results.map { |result| result.created_at.to_date } }.flatten.sort.uniq
-    @results.each do |body_id, results|
-      grouped = @dates.map { |d| [d, results.select { |r| r.created_at.to_date == d }] }.to_h
-      @results[body_id] = grouped
+      scraper_results = body.scraper_results.where(['created_at > ?', @dates.first]).order(created_at: :asc)
+      @results[body.id] = @dates.map { |d| [d, scraper_results.select { |r| r.created_at.to_date == d }] }.to_h
     end
   end
 
