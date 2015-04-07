e46525010c6220cbf2a9cf8940307d5dccc6dfa8
Maximilian Richt <maxi@richt.name>
2015-04-07 19:54:38 +0200

    production: require https in email urls, hardcode smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] || 'kleineanfragen.de' }
+  config.action_mailer.default_url_options = {
+    host: ENV['APP_HOST'] || 'kleineanfragen.de',
+    protocol: 'https'
+  }
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           ENV['SMTP_PORT'].to_i || 25,
     address:        ENV['SMTP_HOST'] || 'localhost',
