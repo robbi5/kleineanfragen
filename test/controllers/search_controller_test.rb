b335013b9bbef833b7883c70c4323d9422bf13ef
Maximilian Richt <maxi@richt.name>
2015-04-20 18:57:29 +0200

    Remove SearchControllerTest, it depends on elasticsearch

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  test "should get search" do
-    get :search
-    assert_response :success
-  end
-
-  test "should get autocomplete" do
-    get :autocomplete
-    assert_response :success
-  end
-
 end
