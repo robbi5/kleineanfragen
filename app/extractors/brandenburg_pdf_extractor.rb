d4fe09ce78b57d18a2f4d1e3523b9060ca7b1f19
Maximilian Richt <maxi@richt.name>
2015-05-07 17:38:52 +0200

    BB: modified pdf extractor again after tests against even more papers

--- app/extractors/brandenburg_pdf_extractor.rb
+++ app/extractors/brandenburg_pdf_extractor.rb
@@ -9,7 +9,7 @@ class BrandenburgPDFExtractor
   # Namens der Landesregierung beantwortet der Minister des Innern und für Kommunales die Kleine Anfrage wie folgt:
   # Namens der Landesregierung beantwortet der Minister der Finanzen die Kleine Anfrage wie folgt:
 
-  ANSWERERS = /Namens\s+der\s+Landesregierung\s+beantwortet\s+(?:der|die)\s+(Minister.+)\s+die\s+(?:Kleine|Große)\s+Anfrage/m
+  ANSWERERS = /Namens\s+der\s+Landesregierung\s+beantwortet\s+(?:der|die)\s+(Minister.+?)\s+die\s+(?:Kleine|Große)\s+An/m
 
   def extract_answerers
     return nil if @contents.nil?
@@ -24,8 +24,10 @@ class BrandenburgPDFExtractor
                .gsub(/\-\n+/, '')
                .gsub("\n", ' ')
                .gsub(/\s+/, ' ')
+               .gsub(/\s+,\s+/, ', ')
                .strip
                .gsub(/\p{Other}/, '') # invisible chars & private use unicode
+               .gsub(/,$/, '')
 
     ministries << ministry unless ministry.blank?
 
