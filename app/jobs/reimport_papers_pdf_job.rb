c18d7b16f6762bb3cd94099bd6d9c0e40c83af0f
Maximilian Richt <maxi@richt.name>
2015-07-01 12:51:23 +0200

    HB: reimport papers too if they contain "Vorläufige, unredigierte Fassung"

--- app/jobs/reimport_papers_pdf_job.rb
+++ app/jobs/reimport_papers_pdf_job.rb
@@ -2,14 +2,20 @@ class ReimportPapersPDFJob < ActiveJob::Base
   queue_as :meta
 
   DISTANCE = 5.days
+  STATES = {
+    'BT' => '%Korrektur\nDrucksache%',
+    'HB' => '%Vorläufige, unredigierte Fassung%'
+  }
 
   def perform
-    bt = Body.find_by_state('BT')
-    papers = Paper.where(body: bt).where(['pdf_last_modified < ?', Date.today - DISTANCE]).where("contents LIKE '%Korrektur\nDrucksache%'")
+    STATES.each do |state, like_query|
+      body = Body.find_by_state(state)
+      papers = Paper.where(body: body).where(['pdf_last_modified < ?', Date.today - DISTANCE]).where('contents LIKE ?', like_query)
 
-    papers.find_each do |paper|
-      logger.info "Adding reimport of Paper [#{paper.body.state} #{paper.full_reference}] to queue"
-      StorePaperPDFJob.perform_later(paper, force: true)
+      papers.find_each do |paper|
+        logger.info "Adding reimport of Paper [#{paper.body.state} #{paper.full_reference}] to queue"
+        StorePaperPDFJob.perform_later(paper, force: true)
+      end
     end
   end
 end
