388d9205b344c08e8d44790b2c38ae5e876ee302
Andreas Worm <andreas.worm@headissue.com>
2015-07-01 13:26:50 +0200

    now with detail scraper and pdf url

--- app/scrapers/sachsen_scraper.rb
+++ app/scrapers/sachsen_scraper.rb
@@ -1,5 +1,6 @@
 module SachsenScraper
   BASE_URL = 'http://edas.landtag.sachsen.de'
+  SEARCH_FIELD = 'ctl00$masterContentCallback$content$suchmaske$tblSearch$tabSuche$panelUmSuchmaskeEinfach$suchmaskeEinfachCallback'
 
   def self.extract_overview_items(page)
     page.search('//td[@class="dxdvItem_EDAS"]/table')
@@ -10,8 +11,16 @@ module SachsenScraper
   end
 
   def self.extract_meta_text(item)
-    t = item.search('.//tr[3]/td').text.split("\n").find{ |t| t.include?('KlAnfr') || t.include?('GrAnfr') }
-    t.strip[6..t.length]
+    text = item.search('.//tr[3]/td').text.split("\n").find{ |t| t.include?('KlAnfr') || t.include?('GrAnfr') }
+    text.strip if !text.nil?
+  end
+
+  def self.extractType(text)
+    if (text[0..5] == 'KlAnfr')
+      Paper::DOCTYPE_MINOR_INTERPELLATION
+    else
+      Paper::DOCTYPE_MAJOR_INTERPELLATION
+    end
   end
 
   def self.extract_date(meta_text)
@@ -24,61 +33,145 @@ module SachsenScraper
 
   def self.extract_originators(meta_text)
     people = [meta_text.partition(/\p{Upper}{2}/)[0].strip]
-    parties = [meta_text.match(/\p{Upper}\p{Upper}+\s*\p{Upper}*/)[0].strip]
+    parties = [meta_text.match(/\p{Upper}\p{Lower}*\p{Upper}+\s*\p{Upper}*/)[0].strip]
     originators = { people: people, parties: parties}
   end
 
   def self.extract_overview_paper(item, doctype)
     meta_text = extract_meta_text item
-    full_reference = extract_reference(meta_text);
+    meta_text = meta_text[6..meta_text.length].strip
+    full_reference = extract_reference(meta_text)
     {
       legislative_term: full_reference.split('/')[0],
       full_reference: full_reference,
       reference: full_reference.split('/')[1],
       doctype: doctype,
       title: extract_title(item),
-      url: '',
+      url: nil,
       published_at: extract_date(meta_text),
       originators: extract_originators(meta_text),
       is_answer: nil,
     }
   end
 
+  def self.extract_detail_paper(item)
+    meta_text = extract_meta_text(item)
+    type = extractType(meta_text)
+    extract_overview_paper(item, type)
+  end
+
+  def self.switchToTerm(i, m, top)
+    nav = top.frame_with(name: 'head').click
+    nav = m.get("/redirect.aspx?wahlperiode=" + i.to_s)
+    nav.forms.first.submit
+  end
+
+  class Detail < DetailScraper
+    def scrape
+      m = mechanize
+      top = m.get BASE_URL
+      SachsenScraper.switchToTerm(@legislative_term, m, top)
+      content = top.frame_with(name: 'content').click
+      content = searchDetail(content)
+      paper = nil
+      SachsenScraper.extract_overview_items(content).each do |item|
+        paper = SachsenScraper.extract_detail_paper(item)
+        content = m.click(item.search('//a').first)
+        answeredDate = content.search('//*[@id="ctl00_masterContentCallback_content_tabTreffer_trefferDataView_IT0_HyperLink6"]').first
+        if (answeredDate.nil? || answeredDate.text.empty?)
+          paper[:is_answer] = false
+        else
+          paper[:is_answer] = true
+          paper[:published_at] = SachsenScraper.extract_date(answeredDate.text)
+          top = m.get(extract_viewer_url(content))
+          pdfurl = extract_pdf_url(top)
+          paper[:url] = pdfurl
+        end
+      end
+      paper
+    end
+
+    def extract_pdf_url(top)
+      nav = top.frame_with(name : 'navigation').click
+      onloadValue = nav.search("//body").first.attribute("onload")
+      pdfurl = onloadValue.to_s[/(http\S*?\.pdf)/]
+    end
+
+    def extract_viewer_url(content)
+      pdfTable = content.search('//table[@id="ctl00_masterContentCallback_content_tabTreffer_trefferDataView_IT0_anzeige_tblButtons"]').first
+      viewerIds = pdfTable.search('//input').select do |i|
+        !i.attribute('name').nil? && !i.attribute('name').value.nil? && !i.attribute('name').value.index(/no\$btn/).nil?
+      end
+      viewerIds = viewerIds.map do |i|
+        i.attribute('name').value
+      end
+      viewerId = viewerIds.last.match(/anzeige\$(.*?)_(.*?)_Drs_(.*?)_no\$btn/)
+      "http://edas.landtag.sachsen.de/viewer.aspx?dok_nr=" + viewerId[1] + "&dok_art=Drs&leg_per=" + viewerId[3] + "&pos_dok="+ viewerId[2]
+    end
+
+    def searchDetail(content)
+      search_form = content.forms.first
+      search_form[SEARCH_FIELD+'$tf_EinfDoknrVon$ec'] = @reference.to_s
+      search_form['__EVENTARGUMENT'] = 'Click'
+      search_form['__EVENTTARGET'] = SEARCH_FIELD+'$btn_EinfSuche'
+      search_form.submit
+    end
+  end
+
   class Overview < Scraper
     SEARCH_URL = BASE_URL + '/parlamentsdokumentation/parlamentsarchiv/dokumententyp.aspx'
 
+    def search(content, type, startAt)
+
+      if (type == Paper::DOCTYPE_MAJOR_INTERPELLATION)
+        type = 'GrAnfr'
+      else
+        type = 'KlAnfr'
+      end
+      search_form = content.forms.first
+      search_form['ctl00_masterContentCallback_content_suchmaske_tblSearch_tabSuche_panelUm' + 'SuchmaskeEinfach_suchmaskeEinfachCallback_sb_EinfDoktyp_ec_VI'] = type
+      search_form['__EVENTARGUMENT'] = 'Click'
+      search_form['__EVENTTARGET'] = SEARCH_FIELD + '$btn_EinfSuche'
+      search_form[SEARCH_FIELD + '$tf_EinfDoknrVon$ec'] = startAt
+      search_form[SEARCH_FIELD + '$tf_EinfDoknrBis$ec'] = 100000
+    content = search_form.submit
+    end
+
+    def supports_pagination?
+      false
+    end
+
     def supports_streaming?
-      true
+      false
+    end
+
+    def scrapeType(m, type, startAt)
+      top = m.get BASE_URL
+      SachsenScraper.switchToTerm(@legislative_term, m, top)
+      content = top.frame_with(name: 'content').click
+      content = search(content, type, startAt)
+      page = 1
+      papers = []
+      while !content.search('//td[@class="dxdvItem_EDAS"]/table')[0].nil? do
+
+        SachsenScraper.extract_overview_items(content).each do |item|
+          paper = SachsenScraper.extract_overview_paper(item, type)
+          papers.push(paper) if !paper.nil?
+        end
+        page += 1
+        content = m.get(BASE_URL + '/parlamentsdokumentation/parlamentsarchiv/trefferliste.aspx?NavSeite=' + page.to_s + '&isHaldeReport=&VolltextSuche=&refferer=')
+      end
+      if (!content.search('//*[@id="ctl00_masterContentCallback_content_trWarning"]')[0].nil?)
+        papers.concat(scrapeType(m, type, papers.last.reference.to_i + 1))
+      end
+      papers
     end
 
     def scrape
       m = mechanize
-      top = m.get 'http://edas.landtag.sachsen.de'
-      content = top.frame_with(name: 'content').click
-      # suche
-      search_form = content.forms.first
-      search_form['ctl00_masterContentCallback_content_suchmaske_tblSearch_tabSuche_panelUmSuchmaskeEinfach_suchmaskeEinfachCallback_sb_EinfDoktyp_ec_VI'] = 'KlAnfr'
-      search_form['__EVENTARGUMENT'] = 'Click'
-      search_form['__EVENTTARGET'] =
-        'ctl00$masterContentCallback$content$suchmaske$tblSearch$tabSuche$panelUmSuchmaskeEinfach$suchmaskeEinfachCallback$btn_EinfSuche'
-      content = search_form.submit
-
-      page = 1
       papers = []
-      while !content.search('//td[@class="dxdvItem_EDAS"]/table').nil? do
-
-        SachsenScraper.extract_overview_items(content).each do |item|
-          paper = SachsenScraper.extract_overview_paper(item, Paper::DOCTYPE_MINOR_INTERPELLATION)
-          puts paper
-          papers.push(paper) if !paper.nil?
-        end
-        page += 1
-        content = m.get('http://edas.landtag.sachsen.de/parlamentsdokumentation/parlamentsarchiv/trefferliste.aspx?NavSeite=' + page.to_s + '&isHaldeReport=&VolltextSuche=&refferer=')
-      end
+      papers.concat(scrapeType(m, Paper::DOCTYPE_MAJOR_INTERPELLATION, 1))
+      papers.concat(scrapeType(m, Paper::DOCTYPE_MINOR_INTERPELLATION, 1))
     end
-  end
-
-  class Detail < DetailScraper
-
   end
 end
