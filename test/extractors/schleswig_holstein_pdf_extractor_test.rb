a012cb68d317c749aaf5137dd892bbfdce9f13d8
Maximilian Richt <maxi@richt.name>
2015-11-11 08:39:56 +0100

    SH: extract ministry with linebreak

--- test/extractors/schleswig_holstein_pdf_extractor_test.rb
+++ test/extractors/schleswig_holstein_pdf_extractor_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class SchleswigHolsteinPDFExtractorTest < ActiveSupport::TestCase
+  PREFIX = "und \n \n\nAntwort \n \nder Landesregierung - "
+  SUFFIX = " \n \n\n"
+
   def paper_with_contents(contents)
     Struct.new(:contents).new(contents)
   end
 
   test 'one ministry (he)' do
-    paper = paper_with_contents("und \n \n\nAntwort \n \nder Landesregierung - Innenminister ")
+    paper = paper_with_contents(PREFIX + 'Innenminister' + SUFFIX)
 
     answerers = SchleswigHolsteinPDFExtractor.new(paper).extract_answerers
 
@@ -15,7 +18,7 @@ class SchleswigHolsteinPDFExtractorTest < ActiveSupport::TestCase
   end
 
   test 'one ministry (she)' do
-    paper = paper_with_contents("und \n \n\nAntwort \n \nder Landesregierung - Ministerin für Bildung und Wissenschaft ")
+    paper = paper_with_contents(PREFIX + 'Ministerin für Bildung und Wissenschaft' + SUFFIX)
 
     answerers = SchleswigHolsteinPDFExtractor.new(paper).extract_answerers
 
@@ -23,8 +26,26 @@ class SchleswigHolsteinPDFExtractorTest < ActiveSupport::TestCase
     assert_equal 'Minister/in für Bildung und Wissenschaft', answerers[:ministries].first
   end
 
+  test 'one ministry (generic)' do
+    paper = paper_with_contents(PREFIX + 'Finanzministerium' + SUFFIX)
+
+    answerers = SchleswigHolsteinPDFExtractor.new(paper).extract_answerers
+
+    assert_equal 1, answerers[:ministries].size
+    assert_equal 'Finanzministerium', answerers[:ministries].first
+  end
+
+  test 'one ministry with linebreak' do
+    paper = paper_with_contents(PREFIX + "Minister für Energiewende, Landwirtschaft, Umwelt und \nländliche Räume" + SUFFIX)
+
+    answerers = SchleswigHolsteinPDFExtractor.new(paper).extract_answerers
+
+    assert_equal 1, answerers[:ministries].size
+    assert_equal 'Minister/in für Energiewende, Landwirtschaft, Umwelt und ländliche Räume', answerers[:ministries].first
+  end
+
   test 'no ministry' do
-    paper = paper_with_contents("und \n \n\nAntwort \n \nder Landesregierung - \n \nVorbemerkung der Landesregierung:")
+    paper = paper_with_contents(PREFIX + "\n \nVorbemerkung der Landesregierung:")
 
     answerers = SchleswigHolsteinPDFExtractor.new(paper).extract_answerers
 
