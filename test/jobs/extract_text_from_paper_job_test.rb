320e742e5845c7ea692341806e097da6cfb2b534
felix <felix.sonnenholzer@headissue.com>
2015-11-25 15:00:16 +0100

    create test/jobs and move job test

--- test/jobs/extract_text_from_paper_job_test.rb
+++ test/jobs/extract_text_from_paper_job_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ExtractTextFromPaperJobTest < ActiveSupport::TestCase
   test 'keep hyphen at wordwrap if it is a name' do
     text = "Beate Walter-\n" +
-           "Rosenheimer"
+           'Rosenheimer'
     # result = "Beate Walter-Rosenheimer\n"
     result_text = ExtractTextFromPaperJob.clean_text(text)
     assert_equal text, result_text
@@ -11,7 +11,7 @@ class ExtractTextFromPaperJobTest < ActiveSupport::TestCase
 
   test 'remove hyphen at wordwrap' do
     text = "be-\n" +
-           "pflanzt"
+           'pflanzt'
     expected = "bepflanzt\n"
     result_text = ExtractTextFromPaperJob.clean_text(text)
     assert_equal expected, result_text
