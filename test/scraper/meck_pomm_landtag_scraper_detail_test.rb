9e091d961f8cf2d9a78ef75b7f9749af6b8a9ff6
felix <felix.sonnenholzer@headissue.com>
2015-11-26 13:23:57 +0100

    MV: fix extract_paper handle exceptional blocks

--- test/scraper/meck_pomm_landtag_scraper_detail_test.rb
+++ test/scraper/meck_pomm_landtag_scraper_detail_test.rb
@@ -52,4 +52,76 @@ class MeckPommLandtagScraperDetailTest < ActiveSupport::TestCase
         }
       }, paper)
   end
+
+  test 'extract details 6/4151 minor without party' do
+    html = Nokogiri::HTML(File.read(Rails.root.join('test/fixtures/meck_pomm_landtag_scraper_detail_6_4151.html')))
+    body = html.search("//table[@id = 'parldokresult']")
+    paper = @scraper.extract(body.at_css('.title'))
+
+    assert_equal(
+      {
+        legislative_term: '6',
+        full_reference: '6/4151',
+        reference: '4151',
+        doctype: Paper::DOCTYPE_MINOR_INTERPELLATION,
+        title: 'Agieren der Staatsanwaltschaft im Zusammenhang mit der öffentlichen Berichterstattung in Sachen „Rabauken-Jäger“',
+        url: 'http://www.dokumentation.landtag-mv.de/Parldok/dokument/36288/agieren-der-staatsanwaltschaft-im-zusammenhang-mit-der-%C3%B6ffentlichen-berichterstattung-in-sachen-%E2%80%9Erabauken-j%C3%A4ger%E2%80%9C.pdf',
+        published_at: Date.parse('Wed, 22 Jul 2015'),
+        originators: {
+          people: ['Barbara Borchardt'],
+          parties: []
+        },
+        answerers: {
+          ministries: ['Justizministerium']
+        }
+      }, paper)
+  end
+
+  test 'extract details major interpellation with Answerers' do
+    html = Nokogiri::HTML(File.read(Rails.root.join('test/fixtures/meck_pomm_landtag_scraper_detail_6_3870.html')))
+    body = html.search("//table[@id = 'parldokresult']")
+    paper = @scraper.extract(body.at_css('.title'))
+
+    assert_equal(
+      {
+        legislative_term: '6',
+        full_reference: '6/3870',
+        reference: '3870',
+        doctype: Paper::DOCTYPE_MAJOR_INTERPELLATION,
+        title: '20 Jahre Verfassung des Landes Mecklenburg-Vorpommern',
+        url: 'http://www.dokumentation.landtag-mv.de/Parldok/dokument/35928/20-jahre-verfassung-des-landes-mecklenburg-vorpommern.pdf',
+        published_at: Date.parse('Tue, 31 Mar 2015'),
+        originators: {
+          people: [],
+          parties: ['DIE LINKE']
+        },
+        answerers: {
+          ministries: ['Staatskanzlei']
+        }
+      }, paper)
+  end
+
+  test 'extract details major interpellation without Answerers' do
+    html = Nokogiri::HTML(File.read(Rails.root.join('test/fixtures/meck_pomm_landtag_scraper_detail_6_2572.html')))
+    body = html.search("//table[@id = 'parldokresult']")
+    paper = @scraper.extract(body.at_css('.title'))
+
+    assert_equal(
+      {
+        legislative_term: '6',
+        full_reference: '6/2572',
+        reference: '2572',
+        doctype: Paper::DOCTYPE_MAJOR_INTERPELLATION,
+        title: 'Linksextremismus in Mecklenburg-Vorpommern',
+        url: 'http://www.dokumentation.landtag-mv.de/Parldok/dokument/34182/linksextremismus-in-mecklenburg-vorpommern.pdf',
+        published_at: Date.parse('Fri, 13 Dec 2013'),
+        originators: {
+          people: [],
+          parties: ['NPD']
+        },
+        answerers: {
+          ministries: []
+        }
+      }, paper)
+  end
 end
