c9ac0a7e2651f83cf5652227c6835fe4b478e185
felix <felix.sonnenholzer@headissue.com>
2015-11-27 10:45:28 +0100

    RP: add pdf extractor for originator parties, fixes #83

---
+++ app/extractors/rheinland_pfalz_pdf_extractor.rb
@@ -0,0 +1,20 @@
+class RheinlandPfalzPDFExtractor
+  def initialize(paper)
+    @contents = paper.contents
+  end
+
+  ORIGINATORS = /K\s*l\s*e\s*i\s*n\s*e\s*A\s*n\s*f\s*r\s*a\s*g\s*e\n\nde[rs]\s+Abgeordneten\s+(.+)\n\nund/m
+
+  def extract_originators
+    return nil if @contents.nil?
+
+    m = @contents.match(ORIGINATORS)
+    names = m[1].gsub(/\p{Z}/, ' ')
+            .gsub("\n", ' ')
+            .gsub(/\s+/, ' ')
+            .strip
+            .gsub(/\p{Other}/, '')
+
+    NamePartyExtractor.new(names).extract
+  end
+end
