fbd38235987cf4e1ab75ea9fbfa5aeaea6c05099
Maximilian Richt <maxi@richt.name>
2016-02-07 21:46:39 +0100

    Keep paper originators unique

--- app/models/paper_originator.rb
+++ app/models/paper_originator.rb
@@ -1,4 +1,6 @@
 class PaperOriginator < ActiveRecord::Base
   belongs_to :paper
   belongs_to :originator, polymorphic: true
+
+  validates_uniqueness_of :originator_id, scope: [:paper_id, :originator_type]
 end
