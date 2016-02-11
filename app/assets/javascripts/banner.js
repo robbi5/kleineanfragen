834ab2be4447d32be51f1b7cb50b156a4c0bb36f
Maximilian Richt <maxi@richt.name>
2016-02-11 19:46:54 +0100

    modify banner styling to match kA font size

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -8,9 +8,9 @@
     '<a id="datenschule_banner_close" style="float:right; text-decoration: none !important;">&times;</a>'+
     'Die <b>OKF DE DATENSCHULE</b> nimmt an der Google Impact Challenge teil. Stimme ab! <a target="_blank" href="https://datenschule.de">datenschule.de</a>'+
     '<style>' +
-    '#datenschule_banner { text-align:center; z-index:99999999; padding:18px; margin: 0; border: 1px solid #ececec; position:fixed; right:4px;' +
-    'font-family: Montserrat, Arial; font-weight: normal; text-decoration: none; font-size: 16px; bottom:0; left:4px; color:#fff !important; background-color:#FF5449 !important; } ' +
-    '#datenschule_banner a {  cursor:pointer; font-size: 16px; font-weight: bold; text-decoration: underline; margin: 0; padding: 0; color:#fff !important; } ' +
+    '#datenschule_banner { text-align:center; z-index:99999999; padding:18px; margin: 0; border: 1px solid #ececec; border-bottom: 0; position:fixed; right:4px;' +
+    'font-family: Montserrat, Arial; font-weight: normal; text-decoration: none; font-size: 14px; bottom:0; left:4px; color:#fff !important; background-color:#FF5449 !important; } ' +
+    '#datenschule_banner a {  cursor:pointer; font-size: 14px; font-weight: bold; text-decoration: underline; margin: 0; padding: 0; color:#fff !important; } ' +
     '#datenschule_banner a:hover,#datenschule_banner a:focus { color:#babab2!important; } ' +
     'a#datenschule_banner_close { font-size: 24px; line-height: 1; } ' +
     '</style>';
