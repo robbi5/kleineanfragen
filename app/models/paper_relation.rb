e0d07f8414b4006746a78a60bf2b4994f1339f26
Maximilian Richt <maxi@richt.name>
2016-02-12 22:10:20 +0100

    relation: add more validations

--- app/models/paper_relation.rb
+++ app/models/paper_relation.rb
@@ -4,4 +4,16 @@ class PaperRelation < ActiveRecord::Base
 
   belongs_to :paper
   belongs_to :other_paper, class_name: 'Paper'
+
+  validates :paper, :other_paper, :reason, presence: true
+  validates :other_paper, uniqueness: { scope: [:paper, :reason] }
+  validate :does_not_link_itself
+
+  private
+
+  def does_not_link_itself
+    if !paper.nil? && !other_paper.nil? && paper.id == other_paper.id
+      errors.add(:other_paper, 'cannot be the same paper')
+    end
+  end
 end
