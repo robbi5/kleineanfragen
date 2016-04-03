7352d2d77274a7f3593f73966d1fd4affab0fcd3
Maximilian Richt <maxi@richt.name>
2016-04-03 15:25:02 +0200

    PuSH: notify for twitter feedformat too

--- app/jobs/notify_push_hub_body_feed_job.rb
+++ app/jobs/notify_push_hub_body_feed_job.rb
@@ -10,6 +10,16 @@ class NotifyPuSHHubBodyFeedJob < ActiveJob::Base
     return if Rails.configuration.x.push_hub.blank?
 
     feed_url = Rails.application.routes.url_helpers.body_feed_url(body: body, format: :atom)
+    urls = [feed_url, "#{feed_url}?feedformat=twitter"]
+
+    urls.each do |url|
+      success = self.class.notify(url)
+      fail "Couldn't notify push hub for url \"#{url}\"" unless success
+    end
+  end
+
+  def self.notify(feed_url)
+    return false if Rails.configuration.x.push_hub.blank?
 
     response = Excon.post(
       Rails.configuration.x.push_hub,
@@ -20,6 +30,6 @@ class NotifyPuSHHubBodyFeedJob < ActiveJob::Base
       headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
     )
 
-    fail "Couldn't get response, status #{response.status}" unless [200, 204].include?(response.status)
+    [200, 204].include?(response.status)
   end
 end
