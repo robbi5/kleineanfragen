d650057b15f8615bfca7e5c7903e7ac273b4fa29
Maximilian Richt <maxi@richt.name>
2016-05-22 17:07:56 +0200

    add matching table regexes to table recognition

--- app/jobs/contains_table_job.rb
+++ app/jobs/contains_table_job.rb
@@ -8,40 +8,48 @@ class ContainsTableJob < PaperJob
 
     fail "No Text for Paper [#{paper.body.state} #{paper.full_reference}]" if paper.contents.blank?
 
-    probability = self.class.recognize(paper.contents)
+    result = self.class.recognize(paper.contents)
 
-    logger.info "Probability of Table(s) in Paper [#{paper.body.state} #{paper.full_reference}]: #{probability}"
+    reason = result[:groups].map(&:to_s).join(',')
 
-    paper.contains_table = probability >= 1
+    logger.info "Probability of Table(s) in Paper [#{paper.body.state} #{paper.full_reference}]: #{result[:probability]} (#{reason})"
+
+    paper.contains_table = result[:probability] >= 1
     paper.save
   end
 
   def self.recognize(contents)
     probability = 0.0
+    groups = []
 
     # Hint 1: "<synonym für nachfolgende> Tabelle"
     if contents.match(/(?:[bB]eiliegende|beigefügte|anliegende|[Ff]olgende|vorstehende|nachstehende|unten stehende)[nr]? Tabelle/)
       probability += 1
+      groups << :synonym
     end
 
     # Hint 2: "Tabelle \d zeigt", "siehe Tabelle \d", "siehe Tabelle in Anlage"
     if contents.match(/Tabelle \d zeigt/) || contents.match(/siehe Tabelle (?:\d|in Anlage)/)
       probability += 1
+      groups << :table_shows
     end
 
     # Hint 3: (Tabelle 1) ... (Tabelle 2)
     if contents.match(/\(Tabelle \d\)/)
       probability += 1
+      groups << :table_parenthesized
     end
 
     # Hint 4: "die Tabellen 1 bis 4"
     if contents.match(/[Dd]ie Tabellen \d bis \d/)
       probability += 1
+      groups << :tables_num
     end
 
     # Hint 5: "\nTabelle 2:\n"
     if contents.match(/\nTabelle \d:\n/)
       probability += 1
+      groups << :table_num
     end
 
     begin
@@ -51,6 +59,7 @@ class ContainsTableJob < PaperJob
         m = contents.scan(/(\d[\p{Zs}\d]+\n[\d][\p{Zs}\d]+)+/m)
         if m
           probability += 0.5 * m.size
+          groups << :looks_like_table_newlines
         end
       end
     rescue => e
@@ -60,6 +69,7 @@ class ContainsTableJob < PaperJob
     # Hint 7: Anlage 3 Tabelle 1, Anlage / Tabelle 1
     if contents.match(/Anlage\s+[\d\/]+\s+Tabelle\s+\d+/m)
       probability += 1
+      groups << :attachment_table
     end
 
     begin
@@ -68,7 +78,10 @@ class ContainsTableJob < PaperJob
         m = contents.scan(/\n([\p{Zs}\S]+?\p{Zs}+(\-?(?>(?:\d{1,3}(?>(?:\.\d{3}))*(?>(?:,\d+)?|\d*\.?\d+))\p{Zs}*)+))\n/m)
         if m
           m.each do |match|
-            probability += 0.5 unless match.first.strip == match.second.strip || match.first.strip.start_with?('vom') || match.first.match('\d{2}\.\d{2}\.\d{4}')
+            unless match.first.strip == match.second.strip || match.first.strip.start_with?('vom') || match.first.match('\d{2}\.\d{2}\.\d{4}')
+              probability += 0.5
+              groups << :looks_like_table_values
+            end
           end
         end
       end
@@ -76,6 +89,9 @@ class ContainsTableJob < PaperJob
       # ignore failure
     end
 
-    probability
+    {
+      probability: probability,
+      groups: groups.uniq
+    }
   end
 end
--- test/jobs/contains_table_job_test.rb
+++ test/jobs/contains_table_job_test.rb
@@ -3,40 +3,40 @@ require 'test_helper'
 class ContainsTableJobTest < ActiveSupport::TestCase
   test 'nachstehende Tabelle' do
     text = 'die nachstehende Tabelle enthält'
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'Tabelle 2 zeigt' do
     text = 'Tabelle 2 zeigt'
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'Daten (Tabelle 3)' do
     text = 'Daten (Tabelle 3)'
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'some numbers' do
     text = "30\n50\n20\n15\n23\n42\n\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'some more numbers like in BE 17/14442' do
     text = "\nU1 96,4 97,1 96,3 98,3 98,3 97,6 96,1 96,0 96,0 \n\n" +
            "U2 96,1 95,1 97,6 97,6 98,1 97,4 97,3 96,8 97,1 \n\n" +
            "U3 98,8 98,2 98,3 99,0 99,4 99,2 99,0 98,8 98,8\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'simple faked table' do
     text = "\nAAA -11.234\nBBB 123\nCCC 1,23\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'month table from BE 17/14442' do
@@ -44,8 +44,8 @@ class ContainsTableJobTest < ActiveSupport::TestCase
            "Juli 2014 32.982  \n\n" +
            "Juli 2014 -20.000 -1.113 -439 -220 -7.722 -7.276 -6.109  \n\n" +
            'Summe 233.217'
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'table from TH 6/1379' do
@@ -54,8 +54,8 @@ class ContainsTableJobTest < ActiveSupport::TestCase
            "25 bis 50 Prozent 17\n" +
            "50 bis 75 Prozent 25\n" +
            "75 bis 100 Prozent 31\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'table from ST 6/4864' do
@@ -67,28 +67,28 @@ class ContainsTableJobTest < ActiveSupport::TestCase
            "Zernitz Milchviehanlage 650\n" +
            "Hohenlepte Elterntierhaltung 19.500\n" +
            "Deetz Milchviehanlage 360 60\n\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_operator probability, :>=, 1
+    result = ContainsTableJob.recognize(text)
+    assert_operator result[:probability], :>=, 1
   end
 
   test 'no table: some text with a date' do
     text = "Antwort des Niedersächsischen Ministeriums für Inneres und Sport namens der Landesregierung\n" +
            " vom 09.12.2015,  \n\n" +
            'gezeichnet '
-    probability = ContainsTableJob.recognize(text)
-    assert_equal 0, probability
+    result = ContainsTableJob.recognize(text)
+    assert_equal 0, result[:probability]
   end
 
   test 'no table: some date on a new line' do
     text = "\n\n  \n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n5.8.2015 \n\n \n\n\n\n 2 \n\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_equal 0, probability
+    result = ContainsTableJob.recognize(text)
+    assert_equal 0, result[:probability]
   end
 
   test 'no table: two date lines' do
     text = "Schriftliche Anfrage\ndes Abgeordneten ...\nvom 14.09.2015\n" +
            "\n\n\nAntwort\ndes Staatsministeriums für ...\nvom 22.10.2015\n"
-    probability = ContainsTableJob.recognize(text)
-    assert_equal 0, probability
+    result = ContainsTableJob.recognize(text)
+    assert_equal 0, result[:probability]
   end
 end
