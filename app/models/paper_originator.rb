65f4ea71e71b713bfa1113c415fe3aeec18354f2
Maximilian Richt <maxi@richt.name>
2016-05-22 13:41:39 +0200

    Validate uniqueness of paper_originator only if all fields are filled

--- app/models/paper_originator.rb
+++ app/models/paper_originator.rb
@@ -2,5 +2,7 @@ class PaperOriginator < ActiveRecord::Base
   belongs_to :paper
   belongs_to :originator, polymorphic: true
 
-  validates_uniqueness_of :originator_id, scope: [:paper_id, :originator_type]
+  validates :originator_id,
+    uniqueness: { scope: [:paper_id, :originator_type] },
+    unless: Proc.new { |o| o.paper_id.blank? || o.originator_type.blank? }
 end
