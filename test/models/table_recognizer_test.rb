a9172f814ac568c98b355acf4bf4f1e9c4d3b05f
Maximilian Richt <maxi@richt.name>
2016-06-06 19:19:08 +0200

    TableRecognizerTest: use custom helper

--- test/models/table_recognizer_test.rb
+++ test/models/table_recognizer_test.rb
@@ -1,95 +1,82 @@
 require 'test_helper'
 
 class TableRecognizerTest < ActiveSupport::TestCase
-  test 'nachstehende Tabelle' do
-    text = 'die nachstehende Tabelle enthält'
+
+  def assert_table(text)
     result = TableRecognizer.new(text).recognize
     assert_operator result[:probability], :>=, 1
+  end
+
+  def assert_no_table(text)
+    result = TableRecognizer.new(text).recognize
+    assert_equal 0, result[:probability]
+  end
+
+  test 'nachstehende Tabelle' do
+    assert_table 'die nachstehende Tabelle enthält'
   end
 
   test 'Tabelle 2 zeigt' do
-    text = 'Tabelle 2 zeigt'
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table 'Tabelle 2 zeigt'
   end
 
   test 'Daten (Tabelle 3)' do
-    text = 'Daten (Tabelle 3)'
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table 'Daten (Tabelle 3)'
   end
 
   test 'some numbers' do
-    text = "30\n50\n20\n15\n23\n42\n\n"
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "30\n50\n20\n15\n23\n42\n\n"
   end
 
   test 'some more numbers like in BE 17/14442' do
-    text = "\nU1 96,4 97,1 96,3 98,3 98,3 97,6 96,1 96,0 96,0 \n\n" +
-           "U2 96,1 95,1 97,6 97,6 98,1 97,4 97,3 96,8 97,1 \n\n" +
-           "U3 98,8 98,2 98,3 99,0 99,4 99,2 99,0 98,8 98,8\n"
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "\nU1 96,4 97,1 96,3 98,3 98,3 97,6 96,1 96,0 96,0 \n\n" +
+                 "U2 96,1 95,1 97,6 97,6 98,1 97,4 97,3 96,8 97,1 \n\n" +
+                 "U3 98,8 98,2 98,3 99,0 99,4 99,2 99,0 98,8 98,8\n"
   end
 
   test 'simple faked table' do
-    text = "\nAAA -11.234\nBBB 123\nCCC 1,23\n"
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "\nAAA -11.234\nBBB 123\nCCC 1,23\n"
   end
 
   test 'month table from BE 17/14442' do
-    text = "Juni 2014 21.844  \n\n" +
-           "Juli 2014 32.982  \n\n" +
-           "Juli 2014 -20.000 -1.113 -439 -220 -7.722 -7.276 -6.109  \n\n" +
-           'Summe 233.217'
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "Juni 2014 21.844  \n\n" +
+                 "Juli 2014 32.982  \n\n" +
+                 "Juli 2014 -20.000 -1.113 -439 -220 -7.722 -7.276 -6.109  \n\n" +
+                 'Summe 233.217'
   end
 
   test 'table from TH 6/1379' do
-    text = "\nStellenanteil Anzahl der Verträge\n" +
-           "unter 25 Prozent 10\n" +
-           "25 bis 50 Prozent 17\n" +
-           "50 bis 75 Prozent 25\n" +
-           "75 bis 100 Prozent 31\n"
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "\nStellenanteil Anzahl der Verträge\n" +
+                 "unter 25 Prozent 10\n" +
+                 "25 bis 50 Prozent 17\n" +
+                 "50 bis 75 Prozent 25\n" +
+                 "75 bis 100 Prozent 31\n"
   end
 
   test 'table from ST 6/4864' do
-    text = "\n\n" +
-           "Zens Schweinemast 2.496 3.768\n" +
-           "Zeppernick Hähnchenmastanlage 75.000\n" +
-           "Zeppernick Hähnchenmastanlage 100.000\n" +
-           "Moritz Rinderanlage 520\n" +
-           "Zernitz Milchviehanlage 650\n" +
-           "Hohenlepte Elterntierhaltung 19.500\n" +
-           "Deetz Milchviehanlage 360 60\n\n"
-    result = TableRecognizer.new(text).recognize
-    assert_operator result[:probability], :>=, 1
+    assert_table "\n\n" +
+                 "Zens Schweinemast 2.496 3.768\n" +
+                 "Zeppernick Hähnchenmastanlage 75.000\n" +
+                 "Zeppernick Hähnchenmastanlage 100.000\n" +
+                 "Moritz Rinderanlage 520\n" +
+                 "Zernitz Milchviehanlage 650\n" +
+                 "Hohenlepte Elterntierhaltung 19.500\n" +
+                 "Deetz Milchviehanlage 360 60\n\n"
   end
 
   test 'no table: some text with a date' do
-    text = "Antwort des Niedersächsischen Ministeriums für Inneres und Sport namens der Landesregierung\n" +
-           " vom 09.12.2015,  \n\n" +
-           'gezeichnet '
-    result = TableRecognizer.new(text).recognize
-    assert_equal 0, result[:probability]
+    assert_no_table "Antwort des Niedersächsischen Ministeriums für Inneres und Sport namens der Landesregierung\n" +
+                    " vom 09.12.2015,  \n\n" +
+                    'gezeichnet '
   end
 
   test 'no table: some date on a new line' do
-    text = "\n\n  \n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n5.8.2015 \n\n \n\n\n\n 2 \n\n"
-    result = TableRecognizer.new(text).recognize
-    assert_equal 0, result[:probability]
+    assert_no_table "\n\n  \n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n5.8.2015 \n\n \n\n\n\n 2 \n\n"
   end
 
   test 'no table: two date lines' do
-    text = "Schriftliche Anfrage\ndes Abgeordneten ...\nvom 14.09.2015\n" +
-           "\n\n\nAntwort\ndes Staatsministeriums für ...\nvom 22.10.2015\n"
-    result = TableRecognizer.new(text).recognize
-    assert_equal 0, result[:probability]
+    assert_no_table "Schriftliche Anfrage\ndes Abgeordneten ...\nvom 14.09.2015\n" +
+                    "\n\n\nAntwort\ndes Staatsministeriums für ...\nvom 22.10.2015\n"
   end
 
   # test 'no table: from file' do
