10d8ba685a1e0614c70f5afed5fbeacc3e3dcdb5
Maximilian Richt <maxi@richt.name>
2016-06-06 19:13:19 +0200

    contains table: move recognition to own class

--- app/jobs/contains_table_job.rb
+++ app/jobs/contains_table_job.rb
@@ -8,7 +8,7 @@ class ContainsTableJob < PaperJob
 
     fail "No Text for Paper [#{paper.body.state} #{paper.full_reference}]" if paper.contents.blank?
 
-    result = self.class.recognize(paper.contents)
+    result = TableRecognizer.new(paper.contents).recognize
 
     reason = result[:groups].map(&:to_s).join(',')
 
@@ -16,82 +16,5 @@ class ContainsTableJob < PaperJob
 
     paper.contains_table = result[:probability] >= 1
     paper.save
-  end
-
-  def self.recognize(contents)
-    probability = 0.0
-    groups = []
-
-    # Hint 1: "<synonym für nachfolgende> Tabelle"
-    if contents.match(/(?:[bB]eiliegende|beigefügte|anliegende|[Ff]olgende|vorstehende|nachstehende|unten stehende)[nr]? Tabelle/)
-      probability += 1
-      groups << :synonym
-    end
-
-    # Hint 2: "Tabelle \d zeigt", "siehe Tabelle \d", "siehe Tabelle in Anlage"
-    if contents.match(/Tabelle \d zeigt/) || contents.match(/siehe Tabelle (?:\d|in Anlage)/)
-      probability += 1
-      groups << :table_shows
-    end
-
-    # Hint 3: (Tabelle 1) ... (Tabelle 2)
-    if contents.match(/\(Tabelle \d\)/)
-      probability += 1
-      groups << :table_parenthesized
-    end
-
-    # Hint 4: "die Tabellen 1 bis 4"
-    if contents.match(/[Dd]ie Tabellen \d bis \d/)
-      probability += 1
-      groups << :tables_num
-    end
-
-    # Hint 5: "\nTabelle 2:\n"
-    if contents.match(/\nTabelle \d:\n/)
-      probability += 1
-      groups << :table_num
-    end
-
-    begin
-      Timeout::timeout(5) do
-        # Hint 6: \d\n\d\n\d\n...
-        #m = contents.scan(/\p{Zs}(\d[\p{Zs}\d]+\n\d[\p{Zs}\d]+)+/m)
-        m = contents.scan(/(\d[\p{Zs}\d]+\n[\d][\p{Zs}\d]+)+/m)
-        if m
-          probability += 0.5 * m.size
-          groups << :looks_like_table_newlines
-        end
-      end
-    rescue => e
-      # ignore failure
-    end
-
-    # Hint 7: Anlage 3 Tabelle 1, Anlage / Tabelle 1
-    if contents.match(/Anlage\s+[\d\/]+\s+Tabelle\s+\d+/m)
-      probability += 1
-      groups << :attachment_table
-    end
-
-    begin
-      Timeout::timeout(5) do
-        # Hint 8: "\nAAA 10,1 10,2 10,3\nBBB 20 21,1 -1.022,2"
-        m = contents.scan(/\n([\p{Zs}\S]+?\p{Zs}+(\-?(?>(?:\d{1,3}(?>(?:\.\d{3}))*(?>(?:,\d+)?|\d*\.?\d+))\p{Zs}*)+))\n/m)
-        if m
-          m.each do |match|
-            unless match.first.strip == match.second.strip || match.first.strip.start_with?('vom') || match.first.match('\d{2}\.\d{2}\.\d{4}')
-              probability += 0.5
-              groups << :looks_like_table_values
-            end
-          end
-        end
-      end
-    rescue => e
-      # ignore failure
-    end
-
-    {
-      probability: probability,
-      groups: groups.uniq
-    }
   end
 end
---
+++ lib/table_recognizer.rb
@@ -0,0 +1,85 @@
+class TableRecognizer
+  attr_reader :text
+  attr_writer :debug
+
+  def initialize(text, debug: false)
+    @text = text
+    @debug = debug
+  end
+
+  def debug?
+    @debug
+  end
+
+  def recognize
+    throw 'No text set' if @text.nil?
+
+    @probability = 0.0
+    @groups = []
+    @matches = []
+
+    # Hint 1: "<synonym für nachfolgende> Tabelle"
+    match(/(?:[bB]eiliegende|beigefügte|anliegende|[Ff]olgende|vorstehende|nachstehende|unten stehende)[nr]? Tabelle/, :synonym)
+
+    # Hint 2: "Tabelle \d zeigt", "siehe Tabelle \d", "siehe Tabelle in Anlage"
+    match(/Tabelle \d zeigt/, :table_shows)
+    match(/siehe Tabelle (?:\d|in Anlage)/, :table_shows)
+
+    # Hint 3: (Tabelle 1) ... (Tabelle 2)
+    match(/\(Tabelle \d\)/, :table_parenthesized)
+
+    # Hint 4: "die Tabellen 1 bis 4"
+    match(/[Dd]ie Tabellen \d bis \d/, :tables_num)
+
+    # Hint 5: "\nTabelle 2:\n"
+    match(/\nTabelle \d:\n/, :table_num)
+
+    begin
+      Timeout::timeout(5) do
+        # Hint 6: \d\n\d\n\d\n...
+        #m = text.scan(/\p{Zs}(\d[\p{Zs}\d]+\n\d[\p{Zs}\d]+)+/m)
+        match(/(\d[\p{Zs}\d]+\n[\d][\p{Zs}\d]+)+/m, :looks_like_table_newlines, factor: 0.5)
+      end
+    rescue => e
+      # ignore failure
+    end
+
+    # Hint 7: Anlage 3 Tabelle 1, Anlage / Tabelle 1
+    match(/Anlage\s+[\d\/]+\s+Tabelle\s+\d+/m, :attachment_table)
+
+    begin
+      Timeout::timeout(5) do
+        # Hint 8: "\nAAA 10,1 10,2 10,3\nBBB 20 21,1 -1.022,2"
+        m = text.scan(/\n([\p{Zs}\S]+?\p{Zs}+(\-?(?>(?:\d{1,3}(?>(?:\.\d{3}))*(?>(?:,\d+)?|\d*\.?\d+))\p{Zs}*)+))\n/m)
+        if m
+          m.each do |match|
+            unless match.first.strip == match.second.strip || match.first.strip.start_with?('vom') || match.first.match('\d{2}\.\d{2}\.\d{4}')
+              @probability += 0.5
+              @groups << :looks_like_table_values
+              @matches << m if debug?
+            end
+          end
+        end
+      end
+    rescue => e
+      # ignore failure
+    end
+
+    {
+      probability: @probability,
+      groups: @groups.uniq,
+      matches: @matches
+    }
+  end
+
+  private
+
+  def match(regex, group, factor: 1)
+    m = text.scan(regex)
+    if !m.blank?
+      @probability += factor * m.size
+      @groups << group
+      @matches << m if debug?
+    end
+  end
+end
