30dfade9eba14a571876d4533a6eab65dc9bbf16
Maximilian Richt <maxi@richt.name>
2016-06-09 22:20:30 +0200

    throw emails to default logger/stdout

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -7,9 +7,17 @@ class EmailProcessor
   #@email.subject
   #@email.body
   def process
+    logger.info @email.inspect
+
     if @email.from[:email].ends_with? '@parlament-berlin.de'
       berlin = Body.find_by_state('BE')
       InstantImportNewPapersJob.perform_later(berlin, berlin.legislative_terms.first)
     end
+  end
+
+  private
+
+  def logger
+    @logger ||= Rails.logger
   end
 end
