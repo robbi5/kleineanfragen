908f2848e317caf08cd858e266efb1408635c4de
Maximilian Richt <maxi@richt.name>
2016-07-20 18:31:55 +0200

    rails5: return false for callback doesn't work anymore, throw :abort

--- app/jobs/paper_job.rb
+++ app/jobs/paper_job.rb
@@ -1,7 +1,4 @@
 class PaperJob < ApplicationJob
-  # override define_callbacks from active_job/callbacks.rb to support termination
-  define_callbacks :perform, terminator: ->(_target, result) { result == false }
-
   # before_perform with a symbol doesn't get job as argument, so wrap it here
   before_perform ->(job) { skip_if_frozen(job) }
 
@@ -9,7 +6,7 @@ class PaperJob < ApplicationJob
     paper = job.arguments.first
     if paper.frozen?
       logger.info "Skipping because Paper [#{paper.body.state} #{paper.full_reference}] is frozen"
-      false
+      throw :abort
     end
   end
 end
