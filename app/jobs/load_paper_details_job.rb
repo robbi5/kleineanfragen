067ca91c14b435340c8e79ca369db5470084067f
Maximilian Richt <maxi@richt.name>
2017-01-23 14:46:09 +0100

    Load Paper Details: fix overwrite if value is false

--- app/jobs/load_paper_details_job.rb
+++ app/jobs/load_paper_details_job.rb
@@ -19,7 +19,7 @@ class LoadPaperDetailsJob < PaperJob
     end
 
     results.each do |key, value|
-      paper.send("#{key}=", value) if paper.send(key).blank? || (OVERWRITEABLE.include?(key) && !value.blank?)
+      paper.send("#{key}=", value) if paper.send(key).blank? || (OVERWRITEABLE.include?(key) && (value === false || !value.blank?))
     end
 
     if !paper.valid?
