943255d4aa98af4f801947f63530886f2741f6ff
Maximilian Richt <maxi@richt.name>
2017-04-06 09:53:25 +0200

    fix broken postgres docker container

--- contrib/postgres-ka/create_db.sh
+++ contrib/postgres-ka/create_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-gosu postgres psql --user postgres <<-EOSQL
-	CREATE DATABASE nomenklatura;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE nomenklatura;
   GRANT ALL PRIVILEGES ON DATABASE nomenklatura TO kleineanfragen;
 EOSQL
