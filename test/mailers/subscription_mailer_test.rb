30615796ba9255e258f46070721e8d7f2f1744d0
Maximilian Richt <maxi@richt.name>
2017-04-17 20:31:03 +0200

    whoops, fix subject test

--- test/mailers/subscription_mailer_test.rb
+++ test/mailers/subscription_mailer_test.rb
@@ -9,7 +9,7 @@ class SubscriptionMailerTest < ActionMailer::TestCase
     subscription = subscriptions(:subscription_active)
     papers = Paper.where(body: Body.find_by_state('BY')).limit(10).order(created_at: :desc).all
     mail = SubscriptionMailer.papers(subscription, papers)
-    assert_equal 'Eine neue kleine Anfrage aus Bayern', mail.subject
+    assert_equal 'Eine neue beantwortete Anfrage aus Bayern', mail.subject
     assert_equal ['test@example.org'], mail.to
     assert_equal [@system_address], mail.from
   end
