7bbd113d637e8e9d731fa6463adcd3b9549e0815
Maximilian Richt <maxi@richt.name>
2018-03-22 20:49:44 +0100

    Body Subscriptions: use created_at OR published_at, because some papers already exist, but are later answered and published

--- app/jobs/send_body_subscriptions_job.rb
+++ app/jobs/send_body_subscriptions_job.rb
@@ -5,7 +5,8 @@ class SendBodySubscriptionsJob < ApplicationJob
     Subscription.body.where(active: true, query: body.state).find_each do |sub|
       # using updated_at, because activation could be delayed by opt_in confirmation
       last_sent_at = sub.last_sent_at || sub.updated_at
-      papers = body.papers.where(['created_at > ?', last_sent_at]).pluck(:id)
+      # use both, because some papers already exist, but are later answered and published
+      papers = body.papers.where(['created_at > ? OR published_at > ?', last_sent_at, last_sent_at]).pluck(:id)
       next if papers.empty?
       # triggers ActionMailers DeliveryJob, see queue :mailer
       SubscriptionMailer.papers(sub, papers).deliver_later
