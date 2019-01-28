a8ed5d5f7bc4b7026272897e572777fa053b7ffb
Maximilian Richt <maxi@richt.name>
2019-01-28 21:07:11 +0100

    Make filter automatic, fixes #112
    Update bootstrap-select to get events. This is used to skip submitting the form if someone is still using other form controls.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,5 @@
+$.fn.selectpicker.Constructor.BootstrapVersion = '3';
+
 document.addEventListener("turbolinks:load", function() {
   var papers = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
@@ -22,10 +24,30 @@ document.addEventListener("turbolinks:load", function() {
     }
   });
 
+  var submitTimeout, previousValue;
+
+  var selectSubmit = function() {
+    $('form.search').submit();
+  };
+
   $('select.selectpicker').not(function(idx, el) {
     return $(el).siblings('.bootstrap-select').size() > 0;
   }).selectpicker({
       iconBase: '',
-      tickIcon: 'selected-tick'
+      tickIcon: 'selected-tick',
+      styleBase: 'btn form-control',
+      showTick: true
+  }).on('show.bs.select', function (el) {
+    if (submitTimeout) clearTimeout(submitTimeout);
+    previousValue = Array.from(this.selectedOptions).map(function(opt){ return opt.value; }).join(',');
+  }).on('hide.bs.select', function (el) {
+    currentValue = Array.from(this.selectedOptions).map(function(opt){ return opt.value; }).join(',');
+    if (previousValue != currentValue) {
+      submitTimeout = setTimeout(selectSubmit, 500);
+    }
+  });
+
+  $('form.search input[type=checkbox]').on('change', function() {
+    submitTimeout = setTimeout(selectSubmit, 500);
   });
 });
