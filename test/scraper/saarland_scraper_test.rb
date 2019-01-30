f58e34421c5c858cc6830759aeeb1d077f2f3af4
Enrico Genauck <kontakt@enricogenauck.de>
2019-01-30 13:44:09 +0100

    Extends saarland_scraper test with some higher level tests
    This is my first point of contact with this projects code.
    Upon inspecting the saarland_scraper test cases I found mostly tests of internal
    and potentially private methods. I didn't feel confident enough to touch the
    scrapers implementation without a high level test perspective of:
    
    - Giving real world derived server response to scraper
    - Checking for extracted entries and their properties
    
    This commit adds webmock to stub the scrapers network calls. That way the used
    fixtures can be more easily checked against the real world responses or updated
    with current data in case the website changes their format again.
    
    references #119

--- test/scraper/saarland_scraper_test.rb
+++ test/scraper/saarland_scraper_test.rb
@@ -3,7 +3,105 @@ require 'test_helper'
 class SaarlandScraperTest < ActiveSupport::TestCase
   def setup
     @scraper = SaarlandScraper
-    @html = Nokogiri::HTML(File.read(Rails.root.join('test/fixtures/sl/overview.html')))
+    @overview = File.read(Rails.root.join('test/fixtures/sl/overview.html'))
+    @html = Nokogiri::HTML(@overview)
+
+    stub_request(:get, "https://www.landtag-saar.de/dokumente/drucksachen")
+      .to_return(
+        status: 200,
+        body: @overview,
+        headers: {
+          "Content-Type": "text/html; charset=ISO-8859-1"
+        }
+      )
+  end
+
+  test '#scrape retrieves all results from saarland server url' do
+    entries = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape
+
+    assert_equal 725, entries.count
+  end
+
+  test '#scrape extracts title' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "Finanzielle Unterstützung von ungewollt kinderlos bleibenden Paaren " +
+      "bei den Kosten einer künstlichen Befruchtung",
+      entry[:title]
+    )
+  end
+
+  test '#scrape extracts legislative_term' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "15",
+      entry[:legislative_term]
+    )
+  end
+
+  test '#scrape extracts reference' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "371",
+      entry[:reference]
+    )
+  end
+
+  test '#scrape extracts full_reference' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "15/371",
+      entry[:full_reference]
+    )
+  end
+
+  test '#scrape extracts doctype' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "written",
+      entry[:doctype]
+    )
+  end
+
+  test '#scrape extracts url' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "https://www.landtag-saar.de/Drucksache/Aw15_0371.pdf",
+      entry[:url]
+    )
+  end
+
+  test '#scrape extracts published_at' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      "Wed, 06 Mar 2013".to_date,
+      entry[:published_at]
+    )
+  end
+
+  test '#scrape extracts is_answer' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      true,
+      entry[:is_answer]
+    )
+  end
+
+  test '#scrape assigns answerers' do
+    entry = @scraper::Overview.new('SOME_LEGISLATIVE_TERM').scrape.first
+
+    assert_equal(
+      {ministries: ["Landesregierung"]},
+      entry[:answerers]
+    )
   end
 
   test 'read json from resultpage' do
