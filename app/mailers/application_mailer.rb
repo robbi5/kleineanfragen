8008dbe85fd19bcd484678f5adb32730ab414565
Maximilian Richt <maxi@richt.name>
2019-04-02 09:02:29 +0200

    add obituary to emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: Rails.configuration.x.email_from # see config/application.rb
   layout 'mailer'
+
+  helper do
+    def display_obituary?
+      Rails.application.config.x.display_obituary
+    end
+  end
 end
